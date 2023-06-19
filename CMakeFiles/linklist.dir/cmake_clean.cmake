08995a189b98165fceda2a0773eea8477555efb2
zhongershun <1411015378@qq.com>
2023-06-19 15:42:24 +0800

    finish testing and checking

---
+++ CMakeFiles/linklist.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linklist.dir/main.cpp.o"
+  "CMakeFiles/linklist.dir/main.cpp.o.d"
+  "linklist"
+  "linklist.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linklist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -18,23 +18,150 @@ void Add(int data, Node *head) {
     // TODO 加锁的插入操作
     // 空指针不能加锁 记得异常处理
     // 分三种情况判断：已经存在、已经位于队尾、正常插入（位于队中）
+    if(head==nullptr){
+        printf("head is null\n");
+        return;
+    }
+    head->mutex_.lock();
+    Node *prev=head;
+    Node *cur=head->next_;
+    while (cur!=nullptr)
+    {
+        cur->mutex_.lock();
+        if (data<cur->data_)
+        {
+            printf("Add %d success\n",data);
+            Node *tmp=new Node(data);
+            // tmp->data_=data;
+            prev->next_=tmp;
+            tmp->next_=cur;
+            cur->mutex_.unlock();
+            prev->mutex_.unlock();
+            return;
+        }
+        else if(data==cur->data_){
+            printf("insert a exist data\n");
+            cur->mutex_.unlock();
+            prev->mutex_.unlock();
+            return;
+        }else{
+            prev->mutex_.unlock();
+            prev=cur;
+            cur=cur->next_;
+        }
+    }
+    printf("Add %d to the end\n",data);
+    Node *tmp=new Node(data);
+    // tmp->data_=data;
+    prev->next_=tmp;
+    prev->mutex_.unlock();
 }
 
 void Add_nolock(int data, Node *head) {
     // TODO 不加锁的插入操作
+    if(head==nullptr){
+        printf("lock a null pointer!\n");
+        return;
+    }
+    // head->mutex_.lock();
+    Node *prev=head;
+    Node *cur=head->next_;
+    while (cur!=nullptr)
+    {
+        // cur->mutex_.lock();
+        if (data<cur->data_)
+        {
+            Node *tmp=new Node(data);
+            // tmp->data_=data;
+            prev->next_=tmp;
+            tmp->next_=cur;
+            // cur->mutex_.unlock();
+            // prev->mutex_.unlock();
+            return;
+        }
+        else if(data==cur->data_){
+            printf("insert a exist data %d\n",data);
+            // cur->mutex_.unlock();
+            // prev->mutex_.unlock();
+            return;
+        }else{
+            // prev->mutex_.unlock();
+            prev=cur;
+            cur=cur->next_;
+        }
+    }
+    printf("Add %d to the end\n",data);
+    Node *tmp=new Node(data);
+    // tmp->data_=data;
+    prev->next_=tmp;
+    // prev->mutex_.unlock();
 }
 
 void Contain(int data, Node *head) {
     // TODO 查找列表中是否含有该数据
+    if(head==nullptr){
+        printf("head is null, Empty list");
+        return;
+    }
+    Node *prev=head;
+    Node *cur=head->next_;
+    while(cur!=nullptr){
+        if(cur->data_==data){
+            printf("%d in list\n",data);
+            return;
+        }else if(cur->data_<data){
+            prev=cur;
+            cur=cur->next_;
+        }else{
+            printf("%d not in list\n",data);
+            return;
+        }
+    }
+    printf("%d not in list\n",data);
+    return;
 }
 
 void Remove(int data, Node *head) {
     // TODO 移除列表中特定数据
+    if(head==nullptr){
+        printf("Empty List, nothing to remove.\n");
+        return;
+    }
+    head->mutex_.lock();
+    Node *prev=head;
+    Node *cur=head->next_;
+    while(cur!=nullptr&&cur->data_<data){
+        cur->mutex_.lock();
+        prev->mutex_.unlock();
+        prev=cur;
+        cur=cur->next_;
+    }
+    if(cur!=nullptr&&cur->data_==data){
+        cur->mutex_.lock();
+        prev->next_=cur->next_;
+        delete cur;
+        prev->mutex_.unlock();
+        printf("Remove %d success\n",data);
+    }else{
+        prev->mutex_.unlock();
+        printf("Remove %d failed, not such data\n",data);
+    }
+    return;
 }
 
 void Print(Node *head) {
     // TODO  遍历打印
     // 只需要锁头节点
+    if(head==nullptr){
+        printf("Empty list, nothing to print\n");
+    }
+    head->mutex_.lock();
+    for(Node*cur=head->next_;cur!=nullptr;cur=cur->next_){
+        printf("%d ",cur->data_);
+    }
+    head->mutex_.unlock();
+    printf("\n");
+    return;
 }
 
 int main() {
@@ -53,7 +180,7 @@ int main() {
 //    for (auto &thread1 : addthreads)
 //        thread1.join();
 //    Print(head);
-//
+
 //    vector<thread> remthreads;
 //    for (int i = 0; i < 100; i++)
 //        remthreads.emplace_back(Remove, i, head);
